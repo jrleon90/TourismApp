e8160d152cf7e557b67b3081cce5007f1e72b2ab
Jose <jrleon90@gmail.com>
2017-02-11 23:42:31 -0500

    insert

--- insert.php
+++ insert.php
@@ -8,7 +8,7 @@
 
   $email = $_POST['EMAIL'];
 if ($email != null && !filter_var($email, FILTER_VALIDATE_EMAIL)){
-  $sql = "insert into users (email) values ('$email')";
+  $sql = "insert into users_app (email) values ('$email')";
 
 if(mysqli_query($con, $sql)){
 echo 'success';
