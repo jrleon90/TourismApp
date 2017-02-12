a54f2f4d3d2be989119d59081363c427cf91b6ad
root <root@Ubuntu-App.localdomain>
2017-02-12 04:44:48 +0000

    Insert bug

--- insert.php
+++ insert.php
@@ -7,7 +7,7 @@
  	$con = mysqli_connect(HOST,USERNAME,PASSWORD,DB);
 
   $email = $_POST['EMAIL'];
-if ($email != null && !filter_var($email, FILTER_VALIDATE_EMAIL)){
+if ($email != null && filter_var($email, FILTER_VALIDATE_EMAIL)){
   $sql = "insert into users_app (email) values ('$email')";
 
 if(mysqli_query($con, $sql)){
